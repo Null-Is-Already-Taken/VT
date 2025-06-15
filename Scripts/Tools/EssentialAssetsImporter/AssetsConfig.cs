fc25e4bae672a99e38ba5843564c7b39a865081e
Vi Tiet <vitiet.programmer@gmail.com>
2025-06-14 23:17:53 -0400

    Update AssetsConfig.cs
    preprocessor directive

--- Scripts/Tools/EssentialAssetsImporter/AssetsConfig.cs
+++ Scripts/Tools/EssentialAssetsImporter/AssetsConfig.cs
@@ -1,4 +1,7 @@
+#if ODIN_INSPECTOR
 using Sirenix.OdinInspector;
+#endif
+
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +10,11 @@ namespace VT.Tools.EssentialAssetsImporter
     [CreateAssetMenu(fileName = "New Assets Config", menuName = "Essential Assets Importer/Assets Config")]
     public class AssetsConfig : ScriptableObject
     {
+#if ODIN_INSPECTOR
         [BoxGroup("Entries")]
         [TableList]
         [HideLabel]
+#endif
         public List<AssetEntry> assetsEntries = new();
     }
 }
